5dd36c92dc222360fd8127a04d4025d08ac7a0fe
wangwei <ronawilliam@163.com>
2019-10-15 00:40:31 +0200

    csv read and write

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Customer;
 import java.util.List;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
