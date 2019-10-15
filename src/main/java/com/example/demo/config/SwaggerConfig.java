ab8eba8d8c0a05f867e1533de6b911048007a307
wangwei <ronawilliam@163.com>
2019-10-15 13:56:37 +0200

    redis config

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -16,15 +16,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("documents for learning swagger").description("describtion").build();
-	}
+  ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title("documents for learning swagger").description("describtion").build();
+  }
 
-	@Bean
-	public Docket customImplementation() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))//// 加了ApiOperation注解的类，才生成接口文档
-				.paths(PathSelectors.any()).build();
-	}
+  @Bean
+  public Docket customImplementation() {
+    return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))//// 加了ApiOperation注解的类，才生成接口文档
+        .paths(PathSelectors.any()).build();
+  }
 
 }
