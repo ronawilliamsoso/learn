32faf4de546a88a3e8613bcbc59729beb2b10f00
Wei Wang <weiwang2702@gmail.com>
2019-11-01 21:07:23 +0100

    volatile atmic

--- src/main/java/com/example/demo/utils/VolatileAtomic.java
+++ src/main/java/com/example/demo/utils/VolatileAtomic.java
@@ -1,38 +1,32 @@
 package com.example.demo.utils;
 
-public class VolatileVisibility{
+public class VolatileAtomic{
 
-	//private static  boolean initFlag = false;
-	private static volatile boolean initFlag = false;
+  private static volatile Integer num = 0;
 
-	public static void main(String[] args) throws InterruptedException{
+  private static void increase(){
+  //private static synchronized void increase(){
+    num++;
+  }
+  public static void main(String[] args) throws InterruptedException{
 
+    Thread[] arrayThread = new Thread[10];
+    for (int i = 0; i < arrayThread.length; i++){
+      arrayThread[i] = new Thread(new Runnable(){
+        @Override
+        public void run(){
+          for (int i = 0; i < 1000; i++){
+            increase();
+          }
+        }
+      });
+      arrayThread[i].start();
+    }
 
-		new Thread(new Runnable(){
-			@Override
-			public void run(){
-
-				//java线程模型：每个线程对共享变量进行了复制，保存在 线程使用的cpu 的缓存里了,别的线程如果进行了修改，本线程是无法感知的，除非用volatile。
-				// Volatile 让 cpu 缓存里的副本互相可见，原理就是，cpu 缓存不保存拷贝副本，直接去主存中访问
-				System.out.println("waiting for the moment initFlag to be true.....");
-				while(!initFlag){
-
-				}
-				// 如果initFlag 不是 volatile，则线程 1 永远无法感知线程 2 对 initflag 进行了修改
-				System.out.println("initFlag is now \"true\"");
-			}
-		}).start();
-
-		Thread.sleep(500);
-
-		new Thread(new Runnable(){
-			@Override
-			public void run(){
-				 initFlag = true;
-			}
-		}).start();
-
-
-	}
+    for (int i = 0; i < arrayThread.length; i++){
+      arrayThread[i].join();
+    }
+    System.out.println("num: " + num); // =====> 不是原子操作：  7678. 加入关键词 synchronized 可以保证得到 10000
+  }
 
 }
