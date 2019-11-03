5ff62f25d635df5be9210e79005cff9c621058d0
Wei Wang <weiwang2702@gmail.com>
2019-11-03 16:57:58 +0100

    AtomicInteger is more that 1 times faster than synchronized

--- src/main/java/com/example/demo/muitiThread/Biaslock.java
+++ src/main/java/com/example/demo/muitiThread/Biaslock.java
@@ -7,7 +7,7 @@ public class Biaslock {
     public static Vector<Integer> vector = new Vector<Integer>();
 
     /**
-     * 偏向锁   ﻿一个线程获得了锁，那么它（大部分程序无锁竞争）再次获得锁的概率非常大，所以去掉锁申请这个过程可以提高执行效率。  30%
+     * 偏向锁   ﻿对于单线程，一个线程获得了锁，那么它（大部分程序无锁竞争）再次获得锁的概率非常大，所以去掉锁申请这个过程可以提高执行效率。  30%
      * 开启偏向锁：-XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0
      * 关闭偏向锁：-XX:-UseBiasedLocking
      * @param args
---
+++ src/main/java/com/example/demo/muitiThread/VolatileAtomic_AtomicInteger.java
@@ -0,0 +1,38 @@
+package com.example.demo.muitiThread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class VolatileAtomic_AtomicInteger{
+
+  private static AtomicInteger   num =  new AtomicInteger(0);
+
+  private static  void increase(){
+    num.getAndIncrement();
+  }
+
+  public static void main(String[] args) throws InterruptedException{
+    long begin = System.currentTimeMillis();
+    Thread[] arrayThread = new Thread[10];
+    for (int i = 0; i < arrayThread.length; i++){
+      arrayThread[i] = new Thread(new Runnable(){
+        @Override
+        public void run(){
+          for (int i = 0; i < 100000; i++){
+            increase();
+          }
+        }
+      });
+      arrayThread[i].start();
+    }
+
+    for (int i = 0; i < arrayThread.length; i++){
+      arrayThread[i].join();
+    }
+
+    long end = System.currentTimeMillis();
+    System.out.println("所花时间： " +(end - begin  ));
+    System.out.println("最终结果num: " + num); // =====> getAndIncrement()是原子操作,比 synchronized 快 一倍以上
+
+  }
+
+}
