3ee0e0b86ef1cca75d1786a9b79cab35a5818c3d
Wei Wang <weiwang2702@gmail.com>
2019-11-22 21:58:46 +0100

    create a new sub project

---
+++ sublearn/src/main/java/com/example/learn/sublearn/sublearn/SublearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn.sublearn.sublearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SublearnApplication{
+
+  public static void main(String[] args){
+    SpringApplication.run(SublearnApplication.class,args);
+  }
+
+}
